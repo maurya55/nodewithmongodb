9f6e6263ecf86e8b9844a4c0998973f2db984855
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-22 14:03:35 +0530

    sall

--- controller.js
+++ controller.js
@@ -15,7 +15,7 @@ module.exports = {
             })
 
             return res.status(200).json({
-                message: "store successfully"
+                message: "store successfully15"
             })
         }
         catch (err) {
