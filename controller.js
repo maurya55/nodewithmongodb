2d75519aa1ed9c6eee3b8119d0d4cfef3c28f0f2
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-23 20:45:02 +0530

    change

--- controller.js
+++ controller.js
@@ -46,7 +46,7 @@ module.exports = {
             await client.disconnect();
             return res.status(200).json({
                 redisstatus,
-                message: "get data ng1 server 5",
+                message: "get data ng1 server",
                 data: getData
             })
         }
